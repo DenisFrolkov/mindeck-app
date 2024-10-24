1d1a9c2181f41bbeda4b349aaecbf750c2963edb
Денис <99652468+DenisFrolkov@users.noreply.github.com>
2024-10-24 21:40:33 +0300

    Добавил модуль data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
 dependencies {
 
     implementation(project(":presentation"))
+    implementation(project(":data"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
