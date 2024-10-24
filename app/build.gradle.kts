ed1d642e3e4529dcf183b5dfdb951bab53b1adf8
Денис <99652468+DenisFrolkov@users.noreply.github.com>
2024-10-24 22:51:17 +0300

    Добавил библиотеку material3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     //Jetpack Compose
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.material3.material3)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- presentation/src/main/java/com/mindeck/presentation/ui/components/FAB.kt
+++
@@ -1,4 +0,0 @@
-package com.mindeck.presentation.ui.components
-
-@Composable
-fun FAB() {}
