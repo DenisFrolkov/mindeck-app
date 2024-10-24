100b08c9934263bd2153c22ea9669e82be9a173b
Денис <99652468+DenisFrolkov@users.noreply.github.com>
2024-10-24 22:33:17 +0300

    Добавил некоторые зависимости в модуль presentation.

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -35,6 +36,11 @@ android {
 dependencies {
     //Подключение модулей
     implementation(project(":domain"))
+
+    //Jetpack Compose
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.material3)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
