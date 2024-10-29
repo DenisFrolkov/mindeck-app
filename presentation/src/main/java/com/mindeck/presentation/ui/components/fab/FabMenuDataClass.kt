919488aef5dd8a4ff5e3e3926c6ccc27baa3af24
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2024-10-29 23:25:32 +0300

    Реализовал класс FabState для управления состоянием FAB и внес улучшения в код.

---
+++ presentation/src/main/java/com/mindeck/presentation/ui/components/fab/FabMenuDataClass.kt
@@ -0,0 +1,5 @@
+package com.mindeck.presentation.ui.components.fab
+
+import androidx.compose.ui.graphics.painter.Painter
+
+data class FabMenuDataClass(val idItem: Int, val text: String, val icon: Painter, val iconContentDescription: String? = "")
