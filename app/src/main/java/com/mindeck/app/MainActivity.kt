a8b1ad7759f355bad1fe83854f04d6acef04fb36
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2024-10-30 18:40:56 +0300

    Реализовал FAB с раскрывающимся меню, анимацией и управлением состоянием.

--- app/src/main/java/com/mindeck/app/MainActivity.kt
+++ app/src/main/java/com/mindeck/app/MainActivity.kt
@@ -4,71 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import com.mindeck.presentation.ui.components.fab.FAB
-import com.mindeck.presentation.ui.components.fab.FabMenuDataClass
-import com.mindeck.presentation.ui.components.fab.FabState
-import com.mindeck.presentation.ui.components.fab.FabState.Companion.ITEM_HEIGHT
-import com.mindeck.presentation.ui.theme.Blue
-import com.mindeck.presentation.ui.theme.White
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val item = listOf(
-                FabMenuDataClass(
-                    0,
-                    "1",
-                    painterResource(com.mindeck.presentation.R.drawable.fab_open_menu_setting_icon)
-                ),
-                FabMenuDataClass(
-                    1,
-                    "1",
-                    painterResource(com.mindeck.presentation.R.drawable.fab_open_menu_setting_icon)
-                ),
-                FabMenuDataClass(
-                    2,
-                    "1",
-                    painterResource(com.mindeck.presentation.R.drawable.fab_open_menu_setting_icon)
-                ),
-                FabMenuDataClass(
-                    3,
-                    "1",
-                    painterResource(com.mindeck.presentation.R.drawable.fab_open_menu_setting_icon)
-                )
-            )
 
-            val fabState = remember { FabState(expandedHeight = (ITEM_HEIGHT * item.size).dp) }
-
-            Box(contentAlignment = Alignment.BottomEnd, modifier = Modifier.fillMaxSize()) {
-                Column() {
-                    Button(onClick = {
-                        if (fabState.isExpanded == true) {
-                            fabState.reset()
-                        }
-                    }) { }
-                    FAB(
-                        fabColor = Blue,
-                        fabIconColor = White,
-                        fabIcon = painterResource(com.mindeck.presentation.R.drawable.fab_menu_icon),
-                        fabMenuItems = item,
-                        fabState = fabState
-                    )
-                    Spacer(modifier = Modifier.height(50.dp))
-                }
-            }
         }
     }
 }
