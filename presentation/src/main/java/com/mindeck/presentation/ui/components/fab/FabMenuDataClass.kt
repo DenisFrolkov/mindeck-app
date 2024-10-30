45c9818736ea555f2e7ae2efd6f2890a6d48a20a
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2024-10-30 18:38:13 +0300

    Добавил файл FabAnimations и вынес в него логику анимации FAB.

--- presentation/src/main/java/com/mindeck/presentation/ui/components/fab/FabMenuDataClass.kt
+++ presentation/src/main/java/com/mindeck/presentation/ui/components/fab/FabMenuDataClass.kt
@@ -2,4 +2,9 @@ package com.mindeck.presentation.ui.components.fab
 
 import androidx.compose.ui.graphics.painter.Painter
 
-data class FabMenuDataClass(val idItem: Int, val text: String, val icon: Painter, val iconContentDescription: String? = "")
+data class FabMenuDataClass(
+    val idItem: Int,
+    val text: String,
+    val icon: Painter,
+    val iconContentDescription: String? = ""
+)
