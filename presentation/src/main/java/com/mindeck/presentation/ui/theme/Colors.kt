8a52d08719926c9aa2fe920c70a1ce65f13e0b57
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2024-11-04 21:57:54 +0300

    Implemented the first version of DropdownSelector with scrollable menu items.

--- presentation/src/main/java/com/mindeck/presentation/ui/theme/Colors.kt
+++ presentation/src/main/java/com/mindeck/presentation/ui/theme/Colors.kt
@@ -3,4 +3,6 @@ package com.mindeck.presentation.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Blue = Color(0xFF3D8FF7)
+val BackgroundScreen = Color(0xFFEEF5FF)
 val White = Color(0xFFFFFFFF)
+val MediumGray = Color(0xFFAEAEAE)
