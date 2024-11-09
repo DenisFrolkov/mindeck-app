a66528be1fdb19109359bcb74d9f67af87813011
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2024-11-09 20:29:37 +0300

    Removed unused code.

--- presentation/src/main/java/com/mindeck/presentation/ui/screens/MainScreen.kt
+++ presentation/src/main/java/com/mindeck/presentation/ui/screens/MainScreen.kt
@@ -1,14 +1,11 @@
 package com.mindeck.presentation.ui.screens
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.mindeck.presentation.R
-import com.mindeck.presentation.ui.components.folder.DisplayCardFolder
 
 @Composable
 fun MainScreen() {
@@ -18,11 +15,6 @@ fun MainScreen() {
             .padding(top = 50.dp)
             .padding(horizontal = 16.dp),
     ) {
-        DisplayCardFolder(
-            numberOfCards = 1,
-            folderName = "Повторите, чтобы не забыть!",
-            folderIcon = R.drawable.repeat_card_item,
-            modifier = Modifier
-        )
+
     }
 }
