5f1b4c723d56ba7b0a18c9d57d2efd572cc1e3d0
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2024-11-09 15:51:25 +0300

    Implemented folder component.

--- presentation/src/main/java/com/mindeck/presentation/ui/screens/MainScreen.kt
+++ presentation/src/main/java/com/mindeck/presentation/ui/screens/MainScreen.kt
@@ -2,15 +2,20 @@ package com.mindeck.presentation.ui.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.mindeck.presentation.ui.components.folder.DisplayCardFolder
 
 @Composable
 fun MainScreen() {
     Box(
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .padding(top = 50.dp)
+            .padding(horizontal = 16.dp),
     ) {
-
+        DisplayCardFolder()
     }
 }
