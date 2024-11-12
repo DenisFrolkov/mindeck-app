6bd63c27419c847abb2703f6877ab652edfab640
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2024-11-12 19:48:53 +0300

    Implemented CardInputField component.

--- presentation/src/main/java/com/mindeck/presentation/ui/components/textfields/TitleCardInputField.kt
+++ presentation/src/main/java/com/mindeck/presentation/ui/components/textfields/TitleCardInputField.kt
@@ -1,14 +1,8 @@
 package com.mindeck.presentation.ui.components.textfields
 
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -34,7 +28,7 @@ fun TitleInputField(
             color = Color.Black,
             fontFamily = FontFamily(Font(fontFamily))
         ),
-        maxLines = 1,
+        singleLine = true,
         decorationBox = { it ->
             if (value.isEmpty()) {
                 Text(
