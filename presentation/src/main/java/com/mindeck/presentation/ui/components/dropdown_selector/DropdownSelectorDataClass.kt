719f241adf09c100914049d635ec14fdfc754ede
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2024-11-17 17:41:50 +0300

    Slightly modified the code.

--- presentation/src/main/java/com/mindeck/presentation/ui/components/dropdown_selector/DropdownSelectorDataClass.kt
+++ presentation/src/main/java/com/mindeck/presentation/ui/components/dropdown_selector/DropdownSelectorDataClass.kt
@@ -3,6 +3,6 @@ package com.mindeck.presentation.ui.components.dropdown_selector
 class DropdownSelectorDataClass(
     val title: String,
     val selectedItem: String,
-    val itemList: List<String>,
+    val itemList: List<String> = listOf(),
     val onItemClick: (String) -> Unit
 )
