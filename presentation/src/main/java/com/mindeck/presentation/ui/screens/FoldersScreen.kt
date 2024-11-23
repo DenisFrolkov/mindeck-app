feec1b5a26d1cebcff2d2be2e778901c24c220a6
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2024-11-23 16:50:17 +0300

    Implemented FoldersScreen.

--- presentation/src/main/java/com/mindeck/presentation/ui/screens/FoldersScreen.kt
+++ presentation/src/main/java/com/mindeck/presentation/ui/screens/FoldersScreen.kt
@@ -3,15 +3,22 @@ package com.mindeck.presentation.ui.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,9 +35,14 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mindeck.presentation.R
 import com.mindeck.presentation.ui.components.buttons.ActionHandlerButton
+import com.mindeck.presentation.ui.components.folder.DisplayCardFolder
+import com.mindeck.presentation.ui.components.folder.FolderData
 import com.mindeck.presentation.ui.theme.BackgroundScreen
 import com.mindeck.presentation.ui.theme.Black
 import com.mindeck.presentation.ui.theme.Blue
+import com.mindeck.presentation.ui.theme.LightBlue
+import com.mindeck.presentation.ui.theme.LightMint
+import com.mindeck.presentation.ui.theme.LimeGreen
 
 @Composable
 fun FoldersScreen() {
@@ -38,30 +50,60 @@ fun FoldersScreen() {
     var textStyle = TextStyle(fontSize = 14.sp, color = Black, fontFamily = fontFamily)
 
     var foldersWord: List<String> = listOf("Папка", "Папки", "Папок")
+    var decksWord: List<String> = listOf("Колода", "Колод", "Колоды")
+
+    val folders = listOf(
+        FolderData(123, "Общая папка", LightBlue, Blue, R.drawable.folder_icon),
+        FolderData(152, "Папка №1", LightBlue, Blue, R.drawable.folder_icon),
+        FolderData(256, "Папка №2", LightBlue, Blue, R.drawable.folder_icon),
+        FolderData(256, "Папка №2", LightBlue, Blue, R.drawable.folder_icon),
+        FolderData(256, "Папка №2", LightBlue, Blue, R.drawable.folder_icon),
+        FolderData(256, "Папка №2", LightBlue, Blue, R.drawable.folder_icon),
+        FolderData(256, "Папка №2", LightBlue, Blue, R.drawable.folder_icon),
+        FolderData(256, "Папка №2", LightBlue, Blue, R.drawable.folder_icon),
+        FolderData(256, "Папка №2", LightBlue, Blue, R.drawable.folder_icon)
+    )
 
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .background(BackgroundScreen)
             .statusBarsPadding()
-            .padding(start = 16.dp, top = 20.dp, end = 16.dp)
+            .navigationBarsPadding()
+            .padding(horizontal = 16.dp)
     ) {
         item {
-            ActionHandlerButton(
-                iconPainter = painterResource(R.drawable.back_icon),
-                contentDescription = stringResource(R.string.back_screen_icon_button),
-                boxModifier = Modifier
-                    .padding(start = 14.dp, top = 14.dp)
-                    .clip(shape = RoundedCornerShape(50.dp))
-                    .clickable(
-                        interactionSource = remember { MutableInteractionSource() },
-                        indication = null
-                    ) { },
-                iconModifier = Modifier
-                    .background(color = Blue, shape = RoundedCornerShape(50.dp))
-                    .padding(all = 12.dp)
-                    .size(size = 16.dp)
-            )
+            Spacer(Modifier.height(34.dp))
+            Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+                ActionHandlerButton(
+                    iconPainter = painterResource(R.drawable.back_icon),
+                    contentDescription = stringResource(R.string.back_screen_icon_button),
+                    boxModifier = Modifier
+                        .clip(shape = RoundedCornerShape(50.dp))
+                        .clickable(
+                            interactionSource = remember { MutableInteractionSource() },
+                            indication = null
+                        ) { },
+                    iconModifier = Modifier
+                        .background(color = Blue, shape = RoundedCornerShape(50.dp))
+                        .padding(all = 12.dp)
+                        .size(size = 16.dp)
+                )
+                ActionHandlerButton(
+                    iconPainter = painterResource(R.drawable.menu_icon),
+                    contentDescription = stringResource(R.string.back_screen_icon_button),
+                    boxModifier = Modifier
+                        .clip(shape = RoundedCornerShape(50.dp))
+                        .clickable(
+                            interactionSource = remember { MutableInteractionSource() },
+                            indication = null
+                        ) { },
+                    iconModifier = Modifier
+                        .background(color = Blue, shape = RoundedCornerShape(50.dp))
+                        .padding(all = 12.dp)
+                        .size(size = 16.dp)
+                )
+            }
             Text(
                 text = stringResource(R.string.title_text_folders),
                 style = textStyle,
@@ -69,14 +111,55 @@ fun FoldersScreen() {
                     .fillMaxWidth()
                     .wrapContentSize(Alignment.Center)
             )
-            Spacer(Modifier.height(24.dp))
-            Text(
-                text = "3 ${getPluralForm(3, foldersWord)}",
-                style = textStyle,
+            Spacer(Modifier.height(18.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
                     .fillMaxWidth()
                     .wrapContentSize(Alignment.Center)
+            ) {
+                Text(
+                    text = "${folders.size} ${getPluralForm(folders.size, foldersWord)}",
+                    style = textStyle,
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Box(
+                    modifier = Modifier
+                        .size(8.dp)
+                        .background(
+                            color = Black, shape = RoundedCornerShape(
+                                50.dp
+                            )
+                        )
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = "347 ${getPluralForm(347, decksWord)}",
+                    style = textStyle,
+                    modifier = Modifier
+                )
+            }
+            Spacer(modifier = Modifier.height(12.dp))
+        }
+        items(folders) {
+            DisplayCardFolder(
+                folderIcon =
+                painterResource(it.icon),
+                numberOfCards = it.countCard,
+                folderName = it.text,
+                backgroundColor = it.color,
+                iconColor = it.colorTwo,
+                onClick = {
+
+                },
+                textStyle = TextStyle(
+                    fontSize = 14.sp,
+                    fontFamily = FontFamily(Font(R.font.opensans_medium))
+                ),
+                modifier = Modifier
             )
+            Spacer(modifier = Modifier.height(6.dp))
         }
     }
 }
