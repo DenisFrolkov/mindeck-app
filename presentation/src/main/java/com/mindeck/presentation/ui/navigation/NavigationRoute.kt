586dd306611a662f3eb8fad018762b4fdae7ab02
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2024-11-24 20:29:44 +0300

    Removed unnecessary code.

--- presentation/src/main/java/com/mindeck/presentation/ui/navigation/NavigationRoute.kt
+++ presentation/src/main/java/com/mindeck/presentation/ui/navigation/NavigationRoute.kt
@@ -3,5 +3,4 @@ package com.mindeck.presentation.ui.navigation
 sealed class NavigationRoute(val route: String) {
     object MainScreen : NavigationRoute("main_screen")
     object CreationCardScreen : NavigationRoute("creation_card_screen")
-    object FolderScreen : NavigationRoute("folder_screen")
 }
