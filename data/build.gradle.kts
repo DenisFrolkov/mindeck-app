b728976cb5947b539a97fa3772f97de57a7fc291
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2024-12-05 15:41:13 +0300

    Updated several libraries: Kotlin to version 2.1.0 and SDK to version 35.

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "com.mindeck.data"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 26
@@ -37,10 +38,17 @@ dependencies {
     //Подключение модулей
     implementation(project(":domain"))
 
+    //Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+
+    //Room
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+    testImplementation(libs.androidx.room.testing)
+    implementation(libs.androidx.room.paging)
+
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
