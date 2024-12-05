d746f0bdce631767e5bd744cc3d20049bc2614d6
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2024-12-05 19:10:08 +0300

    Updated tables and added FolderWithDecks and DeckWithCards relationships.

---
+++ data/src/main/java/com/mimdeck/data/database/entities/DeckWithCards.kt
@@ -0,0 +1,13 @@
+package com.mimdeck.data.database.entities
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+data class DeckWithCards(
+    @Embedded val deck: Deck,
+    @Relation(
+        parentColumn = "deck_id",
+        entityColumn = "deck_id"
+    )
+    val cards: List<Card>
+)
