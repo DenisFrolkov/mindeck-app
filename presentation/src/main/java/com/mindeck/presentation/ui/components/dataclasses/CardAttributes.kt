fc72bb4ecc82b1628e21557ff6c5eefd217a2977
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2025-02-12 14:47:16 +0300

    Improvements made to CardViewModel and CardScreen.

--- presentation/src/main/java/com/mindeck/presentation/ui/components/dataclasses/CardAttributes.kt
+++ presentation/src/main/java/com/mindeck/presentation/ui/components/dataclasses/CardAttributes.kt
@@ -2,5 +2,6 @@ package com.mindeck.presentation.ui.components.dataclasses
 
 data class CardAttributes(
     val title: String,
-    val value: String
+    val value: String? = null,
+    val load: Boolean = false
 )
--- presentation/src/main/java/com/mindeck/presentation/viewmodel/CardViewModel.kt
+++ presentation/src/main/java/com/mindeck/presentation/viewmodel/CardViewModel.kt
@@ -1,9 +1,12 @@
 package com.mindeck.presentation.viewmodel
 
-import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mindeck.domain.models.Card
+import com.mindeck.domain.models.Deck
 import com.mindeck.domain.models.Folder
 import com.mindeck.domain.usecases.cardUseCase.DeleteCardUseCase
 import com.mindeck.domain.usecases.cardUseCase.GetCardByIdUseCase
@@ -12,6 +15,7 @@ import com.mindeck.domain.usecases.cardUseCase.UpdateCardUseCase
 import com.mindeck.presentation.state.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -19,38 +23,47 @@ import javax.inject.Inject
 @HiltViewModel
 class CardViewModel @Inject constructor(
     private val getCardByIdUseCase: GetCardByIdUseCase,
+    private val getFolderByCardIdUseCase: GetFolderByCardIdUseCase,
     private val updateCardUseCase: UpdateCardUseCase,
-    private val deleteCardUseCase: DeleteCardUseCase,
-    private val getFolderByCardIdUseCase: GetFolderByCardIdUseCase
+    private val deleteCardUseCase: DeleteCardUseCase
 ) : ViewModel() {
+    private val _cardByCardIdUIState = MutableStateFlow<UiState<Card>>(UiState.Loading)
+    val cardByCardIdUIState = _cardByCardIdUIState.asStateFlow()
 
-    private val _cardUIState = MutableStateFlow<UiState<Card>>(UiState.Loading)
-    val cardUIState = _cardUIState.asStateFlow()
-
-    private val _folderUIState = MutableStateFlow<UiState<Folder?>>(UiState.Loading)
-    val folderUIState = _folderUIState.asStateFlow()
-
-    fun getCardById(cardId: Int) {
+    fun loadCardById(cardId: Int) {
         viewModelScope.launch {
-            try {
-                val deck = getCardByIdUseCase(cardId = cardId)
-                _cardUIState.value = UiState.Success(deck)
+            _cardByCardIdUIState.value = try {
+                UiState.Success(getCardByIdUseCase(cardId = cardId))
             } catch (e: Exception) {
-                _cardUIState.value = UiState.Error(e)
-                Log.e("CardViewModel", "Error fetching card: ${e.message}")
+                UiState.Error(e)
             }
         }
     }
 
+    private val _folderUIState = MutableStateFlow<UiState<Folder?>>(UiState.Loading)
+    val folderUIState = _folderUIState.asStateFlow()
+
     fun getFolderByCardId(cardId: Int) {
         viewModelScope.launch {
-            try {
-                val folder = getFolderByCardIdUseCase(cardId = cardId)
-                _folderUIState.value =
-                    UiState.Success(folder)
+            _folderUIState.value = try {
+                UiState.Success(getFolderByCardIdUseCase(cardId = cardId))
             } catch (e: Exception) {
-                _folderUIState.value = UiState.Error(e)
-                Log.e("CardViewModel", "Error fetching folder by cardId: ${e.message}")
+                UiState.Error(e)
+            }
+        }
+    }
+
+    private val _deleteCardState = MutableStateFlow<UiState<Unit>>(UiState.Loading)
+    val deleteCardState: StateFlow<UiState<Unit>> = _deleteCardState
+
+    fun deleteDeck(card: Card) {
+        viewModelScope.launch {
+            _deleteCardState.value = try {
+//                deleteCardUseCase(card = card)
+//                UiState.Success(Unit)
+                UiState.Error(Throwable())
+            } catch (e: Exception) {
+                UiState.Error(e)
             }
         }
     }
