18d85be7f030eb5724ab35e3abf5f8b70f76b390
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2025-03-04 19:38:52 +0300

    Implemented work in background using WorkManager and sending notifications.

--- app/src/main/java/com/mindeck/app/App.kt
+++ app/src/main/java/com/mindeck/app/App.kt
@@ -1,7 +1,28 @@
 package com.mindeck.app
 
 import android.app.Application
+import android.util.Log
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.lifecycle.ProcessLifecycleOwner
+import androidx.work.Configuration
+import com.mimdeck.data.AppLifecycleObserver
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class App: Application()
+class App : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun onCreate() {
+        super.onCreate()
+        ProcessLifecycleOwner.get().lifecycle.addObserver(AppLifecycleObserver(applicationContext))
+    }
+
+    override fun getWorkManagerConfiguration() =
+        Configuration.Builder()
+            .setMinimumLoggingLevel(Log.DEBUG)
+            .setWorkerFactory(workerFactory)
+            .build()
+}
---
+++ data/src/main/java/com/mimdeck/data/NotificationWorker.kt
@@ -0,0 +1,67 @@
+package com.mimdeck.data
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.mimdeck.data.database.dao.CardDao
+import androidx.hilt.work.HiltWorker
+import com.mindeck.data.R
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.flow.firstOrNull
+
+@HiltWorker
+class NotificationWorker @AssistedInject constructor(
+    private val cardDao: CardDao,
+    @Assisted appContext: Context,
+    @Assisted workerParameters: WorkerParameters,
+) : CoroutineWorker(appContext, workerParameters) {
+
+    override suspend fun doWork(): Result {
+        return try {
+            val currentTime = System.currentTimeMillis()
+            val card = cardDao.getCardsRepetition(currentTime)
+                .firstOrNull() ?: emptyList()
+
+            if (card.isNotEmpty()) {
+                sendNotification(card.size)
+                Result.success()
+            } else {
+                Result.retry()
+            }
+        } catch (e: Exception) {
+            Result.failure()
+        }
+    }
+
+    private fun sendNotification(count: Int) {
+        val channelId = "card_repetition_channel"
+        val notificationManager =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Повторение карточек",
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                description = "Уведомления о карточках, которые пора повторить"
+            }
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(applicationContext, channelId)
+            .setSmallIcon(R.drawable.notification_icon)
+            .setContentTitle("Пора повторить карточки!")
+            .setContentText("Стоит повторить $count карточек!")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+            .build()
+
+        notificationManager.notify(1, notification)
+    }
+}
