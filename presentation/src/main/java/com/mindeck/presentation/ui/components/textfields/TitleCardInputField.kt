4af302e26c16ae739c6540c1bb709cb12a2d83cd
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2025-03-06 16:41:24 +0300

    Fixed how text input works. Changed the code section responsible for input activation.

--- presentation/src/main/java/com/mindeck/presentation/ui/components/textfields/TitleCardInputField.kt
+++ presentation/src/main/java/com/mindeck/presentation/ui/components/textfields/TitleCardInputField.kt
@@ -1,8 +1,11 @@
 package com.mindeck.presentation.ui.components.textfields
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 
@@ -10,29 +13,38 @@ import androidx.compose.ui.text.TextStyle
 fun TitleInputField(
     placeholder: String,
     value: String,
+    singleLine: Boolean = false,
     onValueChange: (String) -> Unit,
     readOnly: Boolean,
     textStyle: TextStyle,
     placeholderTextStyle: TextStyle,
     modifier: Modifier,
 ) {
-    BasicTextField(
-        value = value,
-        onValueChange = {
-            onValueChange(it)
-        },
-        textStyle = textStyle,
-        singleLine = true,
-        readOnly = readOnly,
-        decorationBox = { it ->
-            if (value.isEmpty()) {
-                Text(
-                    text = placeholder,
-                    style = placeholderTextStyle
-                )
-            }
-            it()
-        },
-        modifier = modifier,
-    )
+    Box(
+        modifier = modifier
+    ) {
+        BasicTextField(
+            value = value,
+            onValueChange = { onValueChange(it) },
+            textStyle = textStyle,
+            singleLine = singleLine,
+            readOnly = readOnly,
+            decorationBox = { innerTextField ->
+                Box(
+                    contentAlignment = Alignment.CenterStart,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    if (value.isEmpty()) {
+                        Text(
+                            text = placeholder,
+                            style = placeholderTextStyle
+                        )
+                    }
+                    innerTextField()
+                }
+            },
+            modifier = Modifier.fillMaxSize()
+        )
+    }
 }
+
