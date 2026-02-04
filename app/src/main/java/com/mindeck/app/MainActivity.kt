7274035074ecf6a25cb0c3ed840033aa0a87e01e
Denis Frolkov <denisfrolkov3@gmail.com>
2026-02-04 22:12:03 +0300

    refactor: migrate to navigation3

--- app/src/main/java/com/mindeck/app/MainActivity.kt
+++ app/src/main/java/com/mindeck/app/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.mindeck.presentation.ui.navigation.AppNavigation
+import com.mindeck.presentation.ui.navigation.MyApp
 import com.mindeck.presentation.ui.theme.MindeckTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MindeckTheme() {
-                AppNavigation()
+                MyApp()
             }
         }
     }
