cce22f4e66beb1b7a24386413cb6abcec82e769b
Denis Frolkov <denisfrolkov3@gmail.com>
2026-05-28 14:20:17 +0300

    feat: add iOS app scaffold

---
+++ iosApp/Mindeck/MindeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindeckApp.swift
+//  iosApp
+//
+//  Created by Denis Frolkov on 5/28/26.
+//
+
+import SwiftUI
+
+@main
+struct MindeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
